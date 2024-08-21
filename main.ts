930400bfed82ceac2c547b60c65d013e3fd182fa
gobeumsu <gobeumsu@gmail.com>
2024-08-21 19:55:44 +0100

    refactor : remove unused import and obsidian template

--- main.ts
+++ main.ts
@@ -1,45 +1,30 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import {
+	Notice,
+	Plugin
+} from "obsidian";
 
 export default class BritishPronunciationPlugin extends Plugin {
-
 	async onload() {
 		this.addCommand({
-			id:'pronounce-slected-text',
-			name: 'Pronounce Selected text',
+			id: "pronounce-slected-text",
+			name: "Pronounce Selected text",
 			editorCallback: (editor) => this.pronouncSelectedText(editor),
-			hotkeys:[]
-
-		})
+			hotkeys: [],
+		});
 	}
-	pronouncSelectedText(editor:any){
+	pronouncSelectedText(editor: any) {
 		const selection = editor.getSelection();
-		if(!selection||selection.toString().trim()=== ''){
-			new Notice('no text selected');
+		if (!selection || selection.toString().trim() === "") {
+			new Notice("no text selected");
 			return;
 		}
-		const utterance = new SpeechSynthesisUtterance(selection)
+		const utterance = new SpeechSynthesisUtterance(selection);
 
 		const voices = speechSynthesis.getVoices();
-		const britishVoice = voices.find(voice =>voice.lang === 'en-GB')
+		const britishVoice = voices.find((voice) => voice.lang === "en-GB");
 		utterance.voice = britishVoice || null;
 		speechSynthesis.speak(utterance);
-	};
-	
-
-	onunload() {
-
 	}
 
+	onunload() {}
 }
-
-
