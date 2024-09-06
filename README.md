0e753bc6d63c7789ec628c40902b44a9c0210c28
gobeumsu <gobeumsu@gmail.com>
2024-09-06 17:07:17 +0100

    refactor : plugin name to pronounce-selected-text

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-# british-pronunciation-plugin
+# Pronounce Selected Text
+
 This plugin for Obsidian allows users to pronounce selected text using a UK English accent. The pronunciation is triggered via a command accessible from the command palette or a custom hotkey.
 
 ## Features
 
-- Pronounces selected text in the Obsidian editor with a UK English accent (`en-GB`).
-- Utilizes the Web Speech API to provide the text-to-speech functionality.
-- Lightweight and integrates seamlessly with Obsidian's native text editor.
+-   Pronounces selected text in the Obsidian editor with a UK English accent (`en-GB`).
+-   Utilizes the Web Speech API to provide the text-to-speech functionality.
+-   Lightweight and integrates seamlessly with Obsidian's native text editor.
+
 ## License
+
 This project is licensed under the MIT License. See the [LICENSE](https://github.com/GoBeromsu/british-pronunciation-plugin/blob/master/LICENSE.md) file for details.
--- main.ts
+++ main.ts
@@ -1,28 +1,30 @@
-import {
-	Notice,
-	Plugin
-} from "obsidian";
+import { Notice, Plugin } from "obsidian";
 
-export default class BritishPronunciationPlugin extends Plugin {
+export default class PronounceSelectedTextPlugin extends Plugin {
 	async onload() {
 		this.addCommand({
-			id: "pronounce-slected-text",
-			name: "Pronounce Selected text",
-			editorCallback: (editor) => this.pronouncSelectedText(editor),
+			id: "pronounce-selected-text",
+			name: "Pronounce selected text",
+			editorCheckCallback: (editor) => this.pronounceSelectedText(editor),
 			hotkeys: [],
 		});
 	}
-	pronouncSelectedText(editor: any) {
+
+	pronounceSelectedText(editor: any) {
 		const selection = editor.getSelection();
 		if (!selection || selection.toString().trim() === "") {
-			new Notice("no text selected");
+			new Notice("No text selected");
 			return;
 		}
 		const utterance = new SpeechSynthesisUtterance(selection);
 
 		const voices = speechSynthesis.getVoices();
 		const britishVoice = voices.find((voice) => voice.lang === "en-GB");
-		utterance.voice = britishVoice || null;
+		if (britishVoice) {
+			utterance.voice = britishVoice;
+		} else {
+			new Notice("British English voice not found");
+		}
 		speechSynthesis.speak(utterance);
 	}
 
